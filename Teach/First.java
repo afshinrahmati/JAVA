44803ad5c513868eb7b5e31954e722e01a77e8d8
afshin <afshinrahmati93@g,mail.com>
2024-12-02 23:14:06 +0330

    second

--- Teach/First.java
+++ Teach/First.java
@@ -1,5 +1,8 @@
 public class First {
-    public static void main(String args) {
+    public static void main(String[] args) {
         System.out.println("salam!!!");
     }
 }
+
+// Compile it with javac ==> java.class
+// Run java with java
